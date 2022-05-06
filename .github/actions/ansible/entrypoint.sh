20c27e7da852368af43dcd74c748859328b8b833
Sahaj8 <sahaj.vaghasiya8112@gmail.com>
2022-05-06 19:20:56 +0530

    ansible added

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Ansible Entrypoint"
+
+echo "[SPE-VM01]" >> /hosts
+echo "20.213.234.81" >> /hosts
+
+echo "[all:vars]" >> /hosts
+echo "ansible_connection=ssh" >> /hosts
+echo "ansible_user= $SSH_USER" >> /hosts
+echo "ansible_ssh_user= $SSH_USER" >> /hosts
+echo "ansible_python_interpreter=/usr/bin/python3.6" >> /hosts
+
+echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
+
+echo "Entering the ansible using ansible-playbook"
+
+ansible-playbook ansible/playbook.yml --user $SSH_USER
