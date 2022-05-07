e4b861cf4513569f7b239523c4c16dfb9daefa0f
Sahaj8 <sahaj.vaghasiya8112@gmail.com>
2022-05-08 01:31:43 +0530

    entrypoint fixed

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -8,11 +8,11 @@ echo "[all:vars]" >> /hosts
 echo "ansible_connection=ssh" >> /hosts
 echo "ansible_user= $SSH_USER" >> /hosts
 echo "ansible_ssh_user= $SSH_USER" >> /hosts
-echo "ansible_python_interpreter=/usr/bin/python3.6" >> /hosts
+echo "ansible_python_interpreter=/usr/bin/python3" >> /hosts
 
 echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
 echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
 echo "Entering the ansible using ansible-playbook"
 
-ansible-playbook ansible/playbook.yml
+ansible-playbook ansible/playbook.yml --user $SSH_USER
