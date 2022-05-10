7a9e1beba3dfe28547266a2d27ad665ed8b8999d
Sahaj8 <sahaj.vaghasiya8112@gmail.com>
2022-05-10 22:21:45 +0530

    sentry add

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { infoLogger } from './logger.js';
 import { errorLogger } from './logger.js';
 
@@ -12,6 +14,27 @@ import userRoutes from './routes/User.js';
 import roomRoutes from './routes/Room.js';
 import activityRoutes from "./routes/activityRoutes.js"
 
+Sentry.init({
+    dsn: "https://7d2fa9f19dde4a25867cad95616ba29a@o1240255.ingest.sentry.io/6392296",
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(bodyParser.json({limit: "30mb", exntended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", exntended: true}));
 app.use(cors());
@@ -20,6 +43,19 @@ app.use("/", mainPageRoutes);
 app.use("/activity", activityRoutes);
 app.use("/users", userRoutes);
 app.use("/rooms", roomRoutes);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(
+    Sentry.Handlers.errorHandler({
+      shouldHandleError(error) {
+        // Capture all 401 and 500 errors
+        if (error.status === 401 || error.status === 500) {
+          return true;
+        }
+        return false;
+      },
+    })
+  );
 
 // const CONNECTION_URL = "mongodb+srv://RoomAllocationAppDB:RoomAllocationAppDB123@cluster0.wzasi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const CONNECTION_URL = "mongodb+srv://Sahajdb:Sahajdb08@cluster0.ashgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
